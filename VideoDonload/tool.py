b3237f2dc7c9507693ef957dcdc2999c4e5d2cdb
nemo <569857936@qq.com>
2019-04-13 00:09:10 +0800

    videospider

--- VideoDonload/tool.py
+++ VideoDonload/tool.py
@@ -105,9 +105,9 @@ def download(osskey, download_url, is_deepimg=False):
             content_size = int(r.headers['content-length'])
 
             if is_deepimg is False:
-                filename =  'VideoSpider/stockpile' + osskey + '.mp4'
+                filename =  osskey + '.mp4'
             elif is_deepimg is True:
-                filename =  'VideoSpider/stockpile' + osskey + 'copy' + '.mp4'
+                filename =   osskey + 'copy' + '.mp4'
 
             with open(filename, "wb") as f:
                 n = 1
