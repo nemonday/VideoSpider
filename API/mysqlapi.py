4b2a7783a32e1358435fcf48e97227568c66f242
nemo <569857936@qq.com>
2019-04-15 16:06:06 +0800

    videospider

--- API/mysqlapi.py
+++ API/mysqlapi.py
@@ -5,18 +5,28 @@ from flask import request
 
 from flask_restful import reqparse, abort, Api, Resource
 
+from API.setting import *
+
 app = Flask(__name__)
 
 api = Api(app)
 
 
+# MYSQL_HOST = 'rm-bp12bzgmvo85rflhio.mysql.rds.aliyuncs.com'
+# MYSQL_PORT = 3306
+# MYSQL_USERNAME = 'migration_test'
+# MYSQL_PASSWORK = 'migrationTest*'
+# MYSQL_DATABASE = 'db_nt_video'
+
 connection = pymysql.connect(
-    MYSQL_HOST='127.0.0.1',
-    MYSQL_PORT = 3306,
-    MYSQL_USERNAME = 'splider',
-    MYSQL_PASSWORK = 'gz_#@splider@2019',
-    MYSQL_DATABASE = 'db_nt_video'
-    )
+    host=MYSQL_HOST,
+    port=MYSQL_PORT,
+    user=MYSQL_USERNAME,
+    password=MYSQL_PASSWORK,
+    db=MYSQL_DATABASE,
+    charset='utf8'
+)
+
 
 
 def match_mysql_keyword(video, video_data):
