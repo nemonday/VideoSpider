73e3aaefef4e140753a3c07f99cf31484fc10cd1
nemo <569857936@qq.com>
2019-04-24 20:25:22 +0800

    videospider

--- VideoMatch/settings.py
+++ VideoMatch/settings.py
@@ -1,15 +1,15 @@
 # mysql 数据库
-# MYSQL_HOST = 'rm-bp12bzgmvo85rflhio.mysql.rds.aliyuncs.com'
-# MYSQL_PORT = 3306
-# MYSQL_USERNAME = 'migration_test'
-# MYSQL_PASSWORK = 'migrationTest*'
-# MYSQL_DATABASE = 'db_nt_video'
-
-MYSQL_HOST = '10.0.0.92'
+MYSQL_HOST = 'rm-bp12bzgmvo85rflhio.mysql.rds.aliyuncs.com'
 MYSQL_PORT = 3306
-MYSQL_USERNAME = 'splider'
-MYSQL_PASSWORK = 'gz_#@splider@2019'
+MYSQL_USERNAME = 'migration_test'
+MYSQL_PASSWORK = 'migrationTest*'
 MYSQL_DATABASE = 'db_nt_video'
+
+# MYSQL_HOST = '10.0.0.92'
+# MYSQL_PORT = 3306
+# MYSQL_USERNAME = 'splider'
+# MYSQL_PASSWORK = 'gz_#@splider@2019'
+# MYSQL_DATABASE = 'db_nt_video'
 
 DOWNLOAD_TIMEOUT = 300
 
