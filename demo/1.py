c5121cd9f7a140ca615fac65a72425bca008fd3a
nemo <569857936@qq.com>
2019-04-25 16:14:27 +0800

    videospider

--- demo/1.py
+++ demo/1.py
@@ -3,19 +3,22 @@ import requests
 url = 'http://10.168.0.115:8080/works/SWorksAuthor/GetAuthorsAuthInfoNew'
 
 
-headers = {
-    "@type" :"type.googleapis.com/ja.common.proto.ReqHead",
-     "ver":"1",
-     "platform":"2",
-     "mbits":"3",
-     "lbits":"4"
-}
+
 
 data = {
-	"offset":0,
-	"count":10000,
+
+    "head" : """"{
+     "@type": "type.googleapis.com/ja.common.proto.ReqHead",
+     "ver":1,
+     "platform":2,
+     "mbits":"3",
+     "lbits":"4"}""",
+    "offset":0,
+    "count":10000,
     "authorLabelType":11
+
+
 }
-res = requests.post(url, headers=headers, data=data)
+res = requests.post(url, data=data)
 print(res.text)
 
