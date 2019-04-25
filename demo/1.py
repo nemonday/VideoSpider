8207fd0f096782ee991811d6dbda17fdbb43a4e4
nemo <569857936@qq.com>
2019-04-25 16:00:29 +0800

    videospider

--- demo/1.py
+++ demo/1.py
@@ -1,8 +1,21 @@
 import requests
 
-url = 'http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=0&city=0&yys=100017&port=1&time=2&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions='
+url = 'http://10.168.0.115:8080/works/SWorksAuthor/GetAuthorsAuthInfoNew'
 
-data = requests.get(url)
 
-print(data.text)
+headers = {
+    "@type" :"type.googleapis.com/ja.common.proto.ReqHead",
+     "ver":"1",
+     "platform":"2",
+     "mbits":"3",
+     "lbits":"4"
+}
+
+data = {
+	"offset":0,
+	"count":10000,
+    "authorLabelType":11
+}
+res = requests.post(url, headers=headers, data=data)
+print(res.text)
 
