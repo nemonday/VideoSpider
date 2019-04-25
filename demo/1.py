c5ab078279fc96c1eb7d9de655233297b3f96214
nemo <569857936@qq.com>
2019-04-25 16:25:34 +0800

    videospider

--- demo/1.py
+++ demo/1.py
@@ -1,15 +1,31 @@
+# import requests
+#
+# url = 'http://10.168.0.115:8080/works/SWorksAuthor/GetAuthorsAuthInfoNew'
+#
+#
+#
+#
+# data = {
+#     'head':'{"@type" :"type.googleapis.com/ja.common.proto.ReqHead","ver":1,"platform":2,"mbits":"3","lbits":"4"}',
+# 	'offset':0,
+# 	'count':10000,
+#     'authorLabelType':11
+#
+#
+# }
+# res = requests.post(url, data=data)
+# print(res)
+
+
 import requests
 
 url = 'http://10.168.0.115:8080/works/SWorksAuthor/GetAuthorsAuthInfoNew'
 
-
-
-
 data = {
-    'head':'{"@type" :"type.googleapis.com/ja.common.proto.ReqHead","ver":1,"platform":2,"mbits":"3","lbits":"4"}',
-	'offset':0,
-	'count':10000,
-    'authorLabelType':11
+    'head':{'@type' :'type.googleapis.com/ja.common.proto.ReqHead','ver':1,'platform':2,'mbits':'3','lbits':'4'},
+     'offset':0,
+     'count':10000,
+        'authorLabelType':11
 
 
 }
