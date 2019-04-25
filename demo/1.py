bd3d08037d3466f5df5b0e4b2b84ab398c6b8af0
nemo <569857936@qq.com>
2019-04-25 16:17:21 +0800

    videospider

--- demo/1.py
+++ demo/1.py
@@ -6,16 +6,10 @@ url = 'http://10.168.0.115:8080/works/SWorksAuthor/GetAuthorsAuthInfoNew'
 
 
 data = {
-
-    "head" : """"{
-     "@type": "type.googleapis.com/ja.common.proto.ReqHead",
-     "ver":1,
-     "platform":2,
-     "mbits":"3",
-     "lbits":"4"}""",
-    "offset":0,
-    "count":10000,
-    "authorLabelType":11
+    'head':'{"@type" :"type.googleapis.com/ja.common.proto.ReqHead","ver":1,"platform":2,"mbits":"3","lbits":"4"}',
+	'offset':0,
+	'count':10000,
+    'authorLabelType':11
 
 
 }
